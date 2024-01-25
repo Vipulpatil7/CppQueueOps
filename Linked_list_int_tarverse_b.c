a68519ee452dd6d292a2bf8e9ac8095d9486893f
NJNXP <nitin_rjadhav@yahoo.com>
2024-01-25 09:34:18 +0530

    Init Single and Double Link List from external value

---
+++ Linked_list_int_tarverse_b.c
@@ -0,0 +1,91 @@
+/*Init Single and Double Link List from external value*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct slist {
+    int dat;
+    struct slist *next;
+};
+
+struct dlist {
+    int dat;
+    struct dlist *prev;
+    struct dlist *next;
+};
+struct slist *shead = NULL;
+
+struct dlist *dhead = NULL;
+struct dlist *current = NULL;
+
+struct slist * insert_slist(struct slist *node, int val)
+{
+    struct slist *ptr= (struct slist *) malloc(sizeof(struct slist));
+
+    ptr->dat=val;
+    ptr->next=NULL;
+    if(shead == NULL)
+    {
+        node= ptr;
+    }
+    else
+    {
+        ptr->next=node;
+        node =ptr;
+        printf("%d ",ptr->dat);
+    }    
+    return node;
+}
+
+struct dlist *  insert_dlist(struct dlist *node, int val)
+{
+    struct dlist *ptr =(struct dlist *)malloc(sizeof(struct dlist));
+    ptr->dat= val;
+    ptr->next=NULL;
+    ptr->prev=NULL;
+    if(current==NULL)
+    {
+        dhead = ptr;
+        current = ptr;
+    }
+    else
+    {
+        ptr->prev=current;
+        current->next=ptr;
+        current =ptr;
+    }
+    return current;
+}
+
+void main(void)
+{
+    for(int i=0; i<5; i++)
+    {
+        shead = insert_slist(shead,i);
+    }
+    printf("\nSingle- ");
+    while(shead != NULL)
+    {
+        printf("%d ",shead->dat);
+        shead= shead->next;
+    }
+
+    for(int i=0; i<5; i++)
+    {
+        current = insert_dlist(current,i);
+    }
+    printf("\nDouble forward- ");
+    while(dhead != NULL)
+    {
+        printf("%d ",dhead->dat);
+        dhead= dhead->next;
+    }
+
+    printf("\nDouble Reverse- ");
+    while(current != NULL)
+    {
+        printf("%d ",current->dat);
+        current= current->prev;
+    }
+
+}
