7a25bfe70fd01e2afc988fd35bb0a6fe70bb6238
NJNXP <nitin_rjadhav@yahoo.com>
2024-01-26 13:50:50 +0530

    Used counter to monitor queue addiition and deletion. Circular queue is oprational

--- queue_array.c
+++ queue_array.c
@@ -7,57 +7,66 @@ struct queue
     int r;
     int size;
     int *arr;
+    int cnt;
 };
 
 int isqueuefull(struct queue *q)
 {
-    if((q->r >= q->size) && q->f<0)
+    if(q->cnt==0)
         return 1;
+    else 
+        return 0;
+}
+
+int isqueueempty(struct queue *q)
+{
+    if(q->cnt==5)
+    {
+        q->r = -1;
+        q->f = -1;
+        return 1;
+    }
     else
         return 0;
 }
 
 void enqueue(struct queue *q, int val)
 {
-    if(isqueuefull(q))
-    {
-        printf("Full\n");
-    }
-    else{
-        if(q->r < q->size)
-            q->r++;
-        else if(q->f >0)
-            q->r = 0;
-        q->arr[q->r] = val;
-        printf("q->r= %d\n",q->r);
-    }    
-}
-int isqueueempty(struct queue *q)
-{
-    if(q->r==q->f)
-        return 1;
-    else
-        return 0;
+    if(q->r < q->size-1)
+        q->r++;
+    else if(q->f >=0)
+        q->r = 0;
+
+    q->arr[q->r] = val;
+    q->cnt--;  
 }
 
 int dequeue(struct queue *q)
 {
     int a=-1;
-    if(isqueueempty(q))
-    {
-        printf("Empty\n");
-    }
+
+    if(q->f <= q->size-1)
+        q->f++;
     else
-    {
-        if(q->f < q->size)
-            q->f++;
-        else
-            q->f = 0;
-       a = q->arr[q->f];
-        printf("q->r= %d\n",q->f);
-    }   
+        q->f = 0;
+
+    a = q->arr[q->f];
+    q->arr[q->f] =-1;
+    q->cnt++;
+    isqueueempty(q); 
     return a; 
 }
+
+void displayqueue(struct queue q)
+{
+    printf("Queue- ");
+    for(int i=0; i<q.size; i++)
+        printf("%d ", q.arr[i]);  
+
+    printf("\nRare Index-  %d\n",q.r);
+    printf("Front Index- %d\n",q.f);     
+}
+
 void main(void)
 {
     int inpt, val;
@@ -66,25 +75,44 @@ void main(void)
     q.arr = (int*)malloc(q.size*sizeof(int));
     q.f=-1;
     q.r=-1;
+    q.cnt=5;
 
     for(int i=0; i<5; i++)
-        enqueue(&q,i);
+        enqueue(&q,i+1);
 
     while(1)
     {
-        printf("\n1. Enqueue\n2. Dequeue\n");
+        printf("\n1. Enqueue\n2. Dequeue\n3. Display Queue\nChoice-");
         scanf("%d",&inpt);
 
         if(inpt == 1)
         {
-            printf("Enter Value= ");
-            scanf("%d",&val);
-            enqueue(&q,val);
+            if(isqueuefull(&q))
+            {
+                printf("Full\n\n");
+            }
+            else
+            {
+                printf("Enter Value= ");
+                scanf("%d",&val);
+                enqueue(&q,val);
+            }
         }
         else if(inpt ==2)
         {
-            val =dequeue(&q);
-            printf("Value= %d",val);
+            if(isqueueempty(&q))
+            {
+                printf("Empty\n\n");
+            }
+            else 
+            {
+                val =dequeue(&q);
+                printf("Value= %d\n\n",val);
+            }
+        }
+        else if(inpt ==3)
+        {
+            displayqueue(q);
         }
         else
         {
